ae5c5fb01b0329312e9c0942b29f876eea2ef4d3
kkkovalov <vlad_kovalov@icloud.com>
2023-12-07 19:25:45 -0800

    currencies header

---
+++ currency.h
@@ -0,0 +1,18 @@
+#ifndef CURRENCY_H
+#define CURRENCY_H
+
+#include <iostream>
+#include <string>
+
+class Currency {
+  public:
+    enum currencies {
+        USD,
+        CAD,
+        GBP,
+        UAH,
+        EUR
+    };
+};
+
+#endif
--- user.h
+++ user.h
@@ -4,5 +4,14 @@
 #include <string>
 #include <iostream>
 
+class User{
+    private:
+        std::string m_id{""};
+    protected:
+        std::string m_name{""};
+        std::string m_timestamp{""};
+    public:
+        User(std::string name, )
+};
 
 #endif
