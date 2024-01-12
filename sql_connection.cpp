6db7b4029d53fae8599f8632652d48d1daf9f912
kkkovalov <vlad_kovalov@icloud.com>
2024-01-11 20:07:23 -0800

    Established MySql connection to C++ via xdevapi

--- sql_connection.cpp
+++ sql_connection.cpp
@@ -1,33 +1,48 @@
+#include <cstdlib>
 #include <iostream>
-#include <mysql/mysql.h>
+#include <mysqlx/xdevapi.h>
+#include <string>
 
-int main() {
-    MYSQL_RES *result;
-    MYSQL_ROW row;
-    MYSQL *connection, mysql;
+using ::std::cout;
+using ::std::endl;
+using namespace mysqlx;
 
-    int state;
+int main(int argc, const char *argv[]) try {
 
-    mysql_init(&mysql);
-
-    connection = mysql_real_connect(&mysql, "127.0.0.1", "root", "AdGjL1029+-", "bank_of_kovalov", 3306, 0, 0);
-
-    if (connection == NULL) {
-        std::cout << mysql_error(&mysql) << '\n';
-    };
-
-    state = mysql_query(connection, "SHOW TABLES");
-    if (state != 0) {
-        std::cout << mysql_error(connection) << '\n';
-    };
-
-    result = mysql_store_result(connection);
-
-    std::cout << "tables: " << mysql_num_rows(result) << std::endl;
-    while ((row = mysql_fetch_row(result)) != NULL) {
-        std::cout << row[0] << std::endl;
+    const char* env_user;
+    const char* env_pswd;
+    if (argc > 2) {
+        env_user = argv[1];
+        env_pswd = argv[2];
+    } else {
+        env_user = "root";
+        env_pswd = NULL;
     }
 
-    mysql_free_result(result);
-    mysql_close(connection);
+    // cout << env_user << '\n' << env_pswd;
+
+    // const char *url = "mysqlx://" + env_user + ":" + env_pswd + "@localhost:33060";
+
+    // cout << "Creating session on " << url << " ..." << endl;
+
+    Session sess("localhost", 33060, env_user, env_pswd);
+
+    cout << "Session accepted, creating collection..." << endl;
+
+    Schema sch = sess.getSchema("bank_of_kovalov");
+    Table transactions = sch.getTable("transactions", true);
+
+    RowResult row = transactions.select().execute();
+
+    std::cout << row.fetchOne().get(1);
+
+} catch (const mysqlx::Error &err) {
+    cout << "ERROR: " << err << endl;
+    return 1;
+} catch (std::exception &ex) {
+    cout << "STD EXCEPTION: " << ex.what() << endl;
+    return 1;
+} catch (const char *ex) {
+    cout << "EXCEPTION: " << ex << endl;
+    return 1;
 }
